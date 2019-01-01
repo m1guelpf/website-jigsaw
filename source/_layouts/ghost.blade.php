02f951084aa357976a1a0aa5aacf043d244c88c7
Miguel Piedrafita <soy@miguelpiedrafita.com>
2019-01-01 22:11:02 +0100

    Add Fathom

--- source/_layouts/ghost.blade.php
+++ source/_layouts/ghost.blade.php
@@ -113,6 +113,20 @@
     <script src="{{ mix('js/main.js', 'assets/build') }}"></script>
     @if($page->production)
         <script src="https://codefund.app/properties/126/funder.js" async="async"></script>
+
+        <script>
+        (function(f, a, t, h, o, m){
+            a[h]=a[h]||function(){
+                (a[h].q=a[h].q||[]).push(arguments)
+            };
+            o=f.createElement('script'),
+            m=f.getElementsByTagName('script')[0];
+            o.async=1; o.src=t; o.id='fathom-script';
+            m.parentNode.insertBefore(o,m)
+        })(document, window, '//analytics.m1guelpf.me/tracker.js', 'fathom');
+        fathom('set', 'siteId', 'LHJWH');
+        fathom('trackPageview');
+        </script>
     @endif
 
     @yield('scripts')
