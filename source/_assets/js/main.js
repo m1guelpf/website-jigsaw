fc345cd9f9ce9a37e1fb50e77ca2626918cdfc8b
Miguel Piedrafita <soy@miguelpiedrafita.com>
2019-01-23 00:58:45 +0100

    Various fixes

--- source/_assets/js/main.js
+++ source/_assets/js/main.js
@@ -42,26 +42,83 @@ for (var i = 0, linksLength = links.length; i < linksLength; i++) {
     }
 }
 
-let themeText = document.getElementById('theme-text');
-let themeChanger = () => {
-    let darkTheme = document.body.classList.contains('global-hash-dark-version');
-    let themeSlider = document.getElementById('theme-slider');
-    document.body.classList.toggle('global-hash-dark-version')
-    
-    if (darkTheme) {
-        themeText.innerText = 'Darken me';
-        localStorage.removeItem('mp-dark')
-        themeSlider.checked = false;
+const setTheme = isDark => {
+    if (isDark) {
+        localStorage.setItem('theme', 'theme_night');
+        document.documentElement.classList.add('global-hash-dark-version');
     } else {
-        themeText.innerText = 'Lighten Up';
-        localStorage.setItem('mp-dark', 'yeeeeeeeah')
-        themeSlider.checked = true;
-     }
-};
-
-if (localStorage.hasOwnProperty('mp-dark')) {
-    themeChanger()
+        document.documentElement.classList.remove('global-hash-dark-version');
+        localStorage.removeItem('theme');
+    }
 }
 
-themeText.onclick = themeChanger
-document.querySelector('.switch > input[type="checkbox"]').onclick = themeChanger
+const toggleTheme = () => {
+    const isDark = !document.documentElement.classList.contains('global-hash-dark-version');
+    setTheme(isDark);
+    document.documentElement.dispatchEvent(
+        new CustomEvent('theme-changed', {
+            bubbles: true,
+            detail: {
+                isDark
+            }
+        }));
+}
+
+const syncThemeToggle = (slider, text) => {
+    const isDark = document.documentElement.classList.contains('global-hash-dark-version');
+    text.textContent = isDark ? 'Lighten Up' : 'Darken me';
+    slider.checked = isDark;
+}
+
+const initThemeToggle = () => {
+    const slider = document.getElementById('theme-slider');
+    const text = document.getElementById('theme-text');
+    syncThemeToggle(slider, text);
+    slider.onclick = text.onclick = () => {
+        toggleTheme();
+        syncThemeToggle(slider, text);
+    };
+}
+
+const contentLoaded = new Promise (resolve => {
+    if (document.readyState === 'loading') {
+        document.addEventListener('DOMContentLoaded', () => resolve());
+    } else {
+        resolve();
+    }
+});
+
+const initTheme = () => {
+    const isDark = localStorage.getItem('theme') === 'theme_night';
+    setTheme(isDark);
+    return contentLoaded.then(initThemeToggle);
+}
+
+initTheme().then(() => {
+    document.body.classList.remove('loading')
+})
+
+
+// let themeText = document.getElementById('theme-text');
+// let themeChanger = () => {
+//     let darkTheme = document.body.classList.contains('global-hash-dark-version');
+//     let themeSlider = document.getElementById('theme-slider');
+//     document.body.classList.toggle('global-hash-dark-version')
+    
+//     if (darkTheme) {
+//         themeText.innerText = 'Darken me';
+//         localStorage.removeItem('mp-dark')
+//         themeSlider.checked = false;
+//     } else {
+//         themeText.innerText = 'Lighten Up';
+//         localStorage.setItem('mp-dark', 'yeeeeeeeah')
+//         themeSlider.checked = true;
+//      }
+// };
+
+// if (localStorage.hasOwnProperty('mp-dark')) {
+//     themeChanger()
+// }
+
+// themeText.onclick = themeChanger
+// document.querySelector('.switch > input[type="checkbox"]').onclick = themeChanger
