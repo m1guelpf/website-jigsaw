5f9fe95f2acfbedb31978754592cb0af5569f39a
Miguel Piedrafita <soy@miguelpiedrafita.com>
2019-02-01 22:57:56 +0100

    Fix rel attribute

--- source/_assets/js/global/links.js
+++ source/_assets/js/global/links.js
@@ -2,6 +2,7 @@ var links = document.links;
 
 for (var i = 0, linksLength = links.length; i < linksLength; i++) {
     if (links[i].hostname != window.location.hostname) {
-        links[i].target = '_blank noopener noreferer';
+        links[i].target = '_blank';
+        links[i].rel = 'noopener noreferer'
     }
 }
