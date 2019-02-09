798db16e441502c7a03165133979552d0d8e95c8
Miguel Piedrafita <soy@miguelpiedrafita.com>
2019-02-09 01:05:36 +0100

    cs

--- functions/newsletter.js
+++ functions/newsletter.js
@@ -1,13 +1,16 @@
 import parse from 'querystring'
 
 exports.handler = (event, context, callback) => {
-    if (event.httpMethod !== 'POST'): callback(null, {
-        statusCode: 301,
-        headers: {
-            Location: 'https://miguelpiedrafita.com'
-        },
-        body: null
-    })
+    if (event.httpMethod !== 'POST') { 
+        callback(null, {
+            statusCode: 301,
+            headers: {
+                Location: 'https://miguelpiedrafita.com'
+            },
+            body: null
+        })
+    }
+
     console.log(parse(event.body))
 
     callback(null, {
