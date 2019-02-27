ccc8f18f75ffcc04efb8d88db503b8433b10778b
Miguel Piedrafita <soy@miguelpiedrafita.com>
2019-02-28 00:50:31 +0100

    Hack to fix weird space caused by statamic ad

--- source/_assets/js/global/spot.js
+++ source/_assets/js/global/spot.js
@@ -1,5 +1,10 @@
  window.addEventListener('scroll', () => {
      var element = document.querySelectorAll('.introduction').length ? document.querySelector('.introduction') : document.querySelector('.section-featured')
+     if (window.scrollY + 10 > Number(getComputedStyle(element).height.replace('px', ''))) {
+         document.body.classList.add('almost-has-statamic');
+     } else {
+         document.body.classList.remove('almost-has-statamic');
+     }
      if (window.scrollY > Number(getComputedStyle(element).height.replace('px', ''))) {
          document.body.classList.add('has-statamic');
      } else {
