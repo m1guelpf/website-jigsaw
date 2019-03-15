70bdd1da4d74cfde7e68fddf12581cbb26edf2f5
Miguel Piedrafita <soy@miguelpiedrafita.com>
2019-03-15 22:45:10 +0100

    Absolute URL for meta images

--- source/_components/meta.blade.php
+++ source/_components/meta.blade.php
@@ -3,7 +3,7 @@
     <meta property="og:title" content="{{ $page->og['title'] ?? $page->title }}" />
     <meta property="og:description" content="{!! $page->og['description'] ?? $page->excerpt !!}" />
     <meta property="og:url" content="{{ $page->getUrl() }}" />
-    <meta property="og:image" content="{{ $page->og['image'] ?? $page->cover_image }}" />
+    <meta property="og:image" content="{{ $page->getUrl() }}{{ $page->og['image'] ?? $page->cover_image }}" />
     <meta property="article:published_time" content="{{ $page->getDate()->format(DATE_ATOM) }}" />
     {{-- <meta property="article:modified_time" content="2018-12-30T22:10:32.000Z" /> --}}
 @foreach($page->tags as $tag)
@@ -14,7 +14,7 @@
     <meta name="twitter:title" content="{{ $page->twitter['title'] ?? $page->title }}" />
     <meta name="twitter:description" content="{{ $page->twitter['description'] ?? $page->excerpt }}" />
     <meta name="twitter:url" content="{{ $page->getUrl() }}" />
-    <meta name="twitter:image" content="{{ $page->twitter['image'] ?? $page->cover_image }}" />
+    <meta name="twitter:image" content="{{ $page->getUrl() }}{{ $page->twitter['image'] ?? $page->cover_image }}" />
     <meta name="twitter:label1" content="Written by" />
     <meta name="twitter:data1" content="{{ $page->siteName }}" />
     <meta name="twitter:label2" content="Filed under" />
